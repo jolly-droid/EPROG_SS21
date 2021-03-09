a6b2898fdcb8ad28d5547e09843166d7520e11c2
julia <julia.autengruber@yahoo.com>
2021-03-09 09:42:07 +0100

    added series 02

---
+++ series01/kreis.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <math.h>
+/*
+ * Schreiben Sie ein Programm, das den Radiusreines Kreises von der Tastatur einliest
+ * und die dazugehorige Fl̈ache sowie den Umfang berechnet und am Bildschirm ausgibt.
+ * Speichern Sie den Source-Code unter kreis.c in das Verzeichnis serie01
+ */
+static int calculateCircumference(int rad){
+    return 2*rad*M_1_PI;
+}
+
+static int calculateArea(int rad){
+    return M_1_PI*rad*rad;
+}
+
+
+int mainM (int argc, char* argv[] ){
+    int k;
+    printf("what size do you want your circle to be? :");
+    scanf("%d", &k);
+    int circ= calculateCircumference(k);
+    int area = calculateArea(k);
+
+    printf("The circumference is %d,\n the area is %d", circ, area);
+    return 0;
+}
+
+
+
