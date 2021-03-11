165c40a91a3f3ccb13161b78d1169dd4ffe1899d
julia <julia.autengruber@yahoo.com>
2021-03-11 19:55:54 +0100

    missing calculation

--- series01/floor.c
+++ series01/floor.c
@@ -1,6 +1,26 @@
 /*
  * he floor function is the function that takes as input a real number and gives as outputthe greatest integer less than or equal to it.
- * For positive numbers, an implementation of this function canbe obtained via the type casting fromdoubletoint.
- * Write a program, which reads a positive numberx >0 from the keyboard, evaluates the floor function, and prints the result to the screen.
- * This approachdoes not work for negative numbers. Why? Save your source code asfloor.cinto the directoryserie01
+ * For positive numbers, an implementation of this function can be obtained via the type casting from double to int.
+ * Write a program, which reads a positive number x >0 from the keyboard, evaluates the floor function, and prints the result to the screen.
+ * This approach does not work for negative numbers. Why? Save your source code asfloor.cinto the directoryserie01
  */
+
+//does not work for negative numbers
+#include <stdio.h>
+
+int mainV(int argc, char* argv[]) {
+    double number;
+    printf("Let's calculate it");
+
+    printf("please insert credit:");
+    scanf("%lf", &number);
+
+    //missing calculation
+
+    double result;
+
+    printf("This is the speaking time: %f", result);
+
+
+    return 0;
+}
