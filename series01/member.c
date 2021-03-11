118dd5402a711e63b5cec4918fb46aeb7ef4a157
julia <julia.autengruber@yahoo.com>
2021-03-11 19:39:57 +0100

    ADDED SOLUTION

--- series01/member.c
+++ series01/member.c
@@ -4,3 +4,15 @@
  * Save your source code asmember.cinto the directoryserie01.
  */
 
+#include <stdio.h>
+
+int mainV(int argc, char* argv[]) {
+    double n;
+    printf("Let's calculate the sequence! ");
+    printf("please insert n:");
+    scanf("%lf", &n);
+    double value = 1/(n+2);
+    printf("This is the value: %f", value);
+
+    return 0;
+}
