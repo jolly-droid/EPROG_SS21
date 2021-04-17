df961022e96e0254de5848e22c402d1c2afb6ad6
julia <julia.autengruber@yahoo.com>
2021-04-17 14:11:48 +0200

    problem wth scanf

---
+++ series04/frobeniusnorm.c
@@ -0,0 +1,41 @@
+#include <assert.h>
+#include <stdio.h>
+#include <math.h>
+double frobeniusnorm(int n, int m, double x[n][m]);
+
+
+int main0407(int argc, char* argv[]) {
+    int n = 3, m = 3;
+    double numberArray[n][m];
+    printf("please insert the matrix:");
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; j++){
+            scanf("%lf", &numberArray[i][j]);
+        }
+    }
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; j++){
+            printf("[%d][%d]: %lf\n",i, j, numberArray[i][j]);
+        }
+    }
+    double result = frobeniusnorm(n, m, numberArray);
+    printf("the result is %lf", result);
+    return 0;
+}
+
+double frobeniusnorm(int n, int m, double x[n][m]){
+
+    double ret = 0, help = 0.5, help2;
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            help2 = pow(x[i][j], 2);
+            ret = ret + help2;
+        }
+
+    }
+
+    ret = pow(ret, help);
+    return ret;
+}
